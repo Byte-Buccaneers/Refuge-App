b0a18951b8131e76d3e194b4e32c9efcc23ad5ed
Kevin Novak <kevinmnovak@yahoo.com>
2015-10-09 23:50:46 -0400

    Replaced with Template

--- Refuge/www/js/index.js
+++ Refuge/www/js/index.js
@@ -1,56 +1,3 @@
-
-
-$(document).ready(function(){
-    //pass the id of a view as a string, in format "#yourview"
-
-    resizeMap();
-    alert("fired ready")
-    if(localStorage.phoneNumber === undefined)
-        switchView("#initialize").delay(1500);
-    else switchView("#map").delay(1500);
-
-    resizeMap: function() {
-         $("#map-canvas").height(Math.max(100,$(window).height()-90));// TODO set
-    }
-
-    var map = L.map('map-canvas').setView([45.423, -75.679], 13);
-
-    //this works, but is online:
-    /*
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18
-    }).addTo(map);
-    */
-
-    //TODO build something to fall back to web if not found.
-    L.tileLayer('img/mapTiles/{z}/{x}/{y}.png', {
-        maxZoom: 17
-    }).addTo(map);
-
-
-    L.marker([45.423, -75.679]).addTo(map)
-        .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-    var popup = L.popup();
-
-    function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
-    }
-
-    map.on('click', onMapClick);
-});
-
-
-
-function switchView(viewID){
-    alert("fired switch")
-    $(".view active").fadeOut(300, function(){
-        $(viewID).css("display","block")
-    });
-}
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -69,3 +16,33 @@ function switchView(viewID){
  * specific language governing permissions and limitations
  * under the License.
  */
+var app = {
+    SOME_CONSTANTS : false,  // some constant
+
+
+    // Application Constructor
+    initialize: function() {
+        console.log("console log init");
+        this.bindEvents();
+        this.initFastClick();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    initFastClick : function() {
+        window.addEventListener('load', function() {
+            FastClick.attach(document.body);
+        }, false);
+    },
+    // Phonegap is now ready...
+    onDeviceReady: function() {
+        console.log("device ready, start making you custom calls!");
+
+        // Start adding your code here....
+
+    }
+};
